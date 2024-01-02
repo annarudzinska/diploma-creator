b42dc7afed7b135ecff385fb3e19390cda0b0bf9
Anna Rudzinska <a.rudzinska97@gmail.com>
2024-01-02 16:31:23 +0100

    adding two selectors and fixing code struture

--- src/components/selectors.tsx
+++ src/components/selectors.tsx
@@ -4,42 +4,44 @@ import Autocomplete from '@mui/material/Autocomplete';
 
 interface Props {
     onHeaderTextChange: (text: string) => void;
-};
+}
 
 export const Selectors: React.FC<Props> = (props) => {
-    const [headerText, setHeaderText] = React.useState<string>('');
+	const [headerText, setHeaderText] = React.useState<string>('');
 
-    const handleHeadeTextChange = React.useCallback(
-        (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-            setHeaderText(event.target.value);
-            props.onHeaderTextChange(event.target.value)
-        },
-        [setHeaderText, props.onHeaderTextChange]
-    );
-    const autocompleteOptions = React.useMemo(
-        () => {
-            return [
-                'Times New Roman',
-                'Arial',
-                'Verdana',
-                'Comic Sans'
-            ]
-        },
-        []
-    );
+	const handleHeadeTextChange = React.useCallback(
+		(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+			setHeaderText(event.target.value);
+			props.onHeaderTextChange(event.target.value);
+		},
+		[setHeaderText, props.onHeaderTextChange]
+	);
+	const autocompleteOptions = React.useMemo(
+		() => {
+			return [
+				'Times New Roman',
+				'Arial',
+				'Verdana',
+				'Comic Sans'
+			]
+		},
+		[]
+	);
 
-    return (
-        <div>
-            <TextField
-                label='Header text'
-                value={headerText}
-                onChange={handleHeadeTextChange}
-                defaultValue='This is header'
-            />
-            <Autocomplete
-                options={autocompleteOptions}
-                renderInput={(params) => <TextField {...params} label="Movie" />}
-            />
-        </div>
-    )
-}
+	return (
+		<div>
+			<TextField
+				label='Header text'
+				value={headerText}
+				onChange={handleHeadeTextChange}
+				defaultValue='This is header'
+                sx={{width: 300}}
+			/>
+			<Autocomplete
+				sx={{width: 300}}
+				options={autocompleteOptions}
+				renderInput={(params) => <TextField {...params} label="Font" />}
+			/>
+		</div>
+	);
+};
