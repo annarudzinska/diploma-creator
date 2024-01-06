16ce1f1ec59b3968a68e1daf3ab3e11322b51ada
Anna Rudzinska <a.rudzinska97@gmail.com>
2024-01-06 23:34:26 +0100

    Creating canvas and making selectors more adjustable

--- src/components/selectors.tsx
+++ src/components/selectors.tsx
@@ -1,46 +1,73 @@
 import React from "react";
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import { Typography } from "@mui/material";
+
+
+type SelectorType = 'Header' | 'Text' | 'Footer';
 
 interface Props {
-    onHeaderTextChange: (text: string) => void;
+	onTextChange: (text: string) => void;
+	onFontSizeChange: (number: string) => void;
+	type: SelectorType;
 }
 
 export const Selectors: React.FC<Props> = (props) => {
-	const [headerText, setHeaderText] = React.useState<string>('');
+	const [text, setText] = React.useState<string>('');
+	const [fontSize, setFontSize] = React.useState<string>('');
 
-	const handleHeadeTextChange = React.useCallback(
+
+	const handleTextChange = React.useCallback(
 		(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-			setHeaderText(event.target.value);
-			props.onHeaderTextChange(event.target.value);
+			setText(event.target.value);
+			props.onTextChange(event.target.value);
 		},
-		[setHeaderText, props.onHeaderTextChange]
+		[setText, props.onTextChange]
 	);
-	const autocompleteOptions = React.useMemo(
+
+	const handleFontSizeChange = React.useCallback(
+		(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+			setFontSize(event.target.value);
+			props.onFontSizeChange(event.target.value);
+		},
+		[setFontSize, props.onFontSizeChange]
+	);
+
+	const fontAutocompleteOptions = React.useMemo(
 		() => {
 			return [
 				'Times New Roman',
 				'Arial',
 				'Verdana',
 				'Comic Sans'
-			]
+			];
 		},
 		[]
 	);
 
 	return (
 		<div>
+			<Typography variant='h6' fontFamily='Arial'>
+				{props.type} selectors
+			</Typography>
 			<TextField
-				label='Header text'
-				value={headerText}
-				onChange={handleHeadeTextChange}
-				defaultValue='This is header'
-                sx={{width: 300}}
+				label={`${props.type} content`}
+				value={text}
+				onChange={handleTextChange}
+				defaultValue={`${props.type} content goes here`}
+				sx={{ width: 300 }}
 			/>
 			<Autocomplete
-				sx={{width: 300}}
-				options={autocompleteOptions}
+				sx={{ width: 300 }}
+				options={fontAutocompleteOptions}
 				renderInput={(params) => <TextField {...params} label="Font" />}
+			/>
+			<TextField
+				label='Font size'
+				value={fontSize}
+				onChange={handleFontSizeChange}
+				defaultValue={`${props.type} font size is set here`}
+				sx={{ width: 100 }}
 			/>
 		</div>
 	);
